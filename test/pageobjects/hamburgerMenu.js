02529b30fe386688885ce621040d837dc2642288
audreyChecketts <achkts@gmail.com>
2024-11-14 08:33:35 -0700

    getters for menu and one user

--- test/pageobjects/hamburgerMenu.js
+++ test/pageobjects/hamburgerMenu.js
@@ -3,14 +3,44 @@ import SauceBasePage from './basePage.js';
 
 class HamburgerMenu extends SauceBasePage {
 
-get hamburgerMenu () {
-    return $('#react-burger-menu-btn');
+    get hamburgerMenu () {
+        return $('#react-burger-menu-btn');
+    }
+
+    get hamburgerMenuClose () {
+        return $('#react-burger-cross-btn');
+    }
+
+    get hamburgerMenuAllItems () {
+        return $('#inventory_sidebar_link');
+    }
+
+    get hamburgerMenuAbout () {
+        return $('#about_sidebar_link');
+    }
+
+    get hamburgerMenuLogOut() {
+        return $('#logout_sidebar_link');
+    }
+
+    get hamburgerMenuReset () {
+        return $('#reset_sidebar_link');
+    }
+
+
+
+    async hamburgerMenuSelect (){
+        await this.hamburgerMenu.click();
+    }
+
+    async hamburgerAllItems (){
+        await this.hamburgerMenu.click();
+        await this.hamburgerMenuClose.click();
+    }
+
+
 }
 
-get logOut() {
-    return $('#logout_sidebar_link');
-}
-}
 
 
 export default new HamburgerMenu();
