bda62f535706b489ad96a191ba35c72cc40e52aa
AudreyChecketts <audreychecketts@gmail.com>
2024-11-19 09:07:31 -0700

    changed

--- test/pageobjects/users.js
+++ test/pageobjects/users.js
@@ -2,7 +2,7 @@ export const standardUser = {username: 'standard_user', password: 'secret_sauce'
 export const validCustomer = {fname: 'Roger', lname: 'Thompson'};
 export const blankSpaceCustomer = {fname: ' ', lname: ' '};
 export const nonsenseCustomer = {fname: 'asdfweweoifjwoeif298423752', lnamd: '2385798hfhw fsbdf'};
-export const postalCode = '78250';
+export const postalCode = {pCode: '78250'};
 export const blankSpacePostalCode = ' ';
 export const wordyPostalCode = 'asfwejfo32';
 
